804d1328479bb8b47ae242e1eec503710c686c2f
nobodyknowsdotcom <nobodyknowsdotcom@gmail.com>
2023-03-12 00:39:51 +0500

    Add application template

---
+++ src/main/java/com/example/eventsplatformbackend/EventsPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventsplatformbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsPlatformBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventsPlatformBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eventsplatformbackend/EventsPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventsplatformbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsPlatformBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
