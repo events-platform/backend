dc9b1001ab9ecd0c07fd24992ad7306a1cbd7594
Dmitry Medvedev <myclosestisa@gmail.com>
2023-04-15 17:31:55 +0500

    Add password changing feature

---
+++ src/main/java/com/example/eventsplatformbackend/dto/PasswordChangeDto.java
@@ -0,0 +1,15 @@
+package com.example.eventsplatformbackend.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class PasswordChangeDto {
+    @NotBlank
+    @Size(min = 8, message = "password cannot be shorter, than 8 characters")
+    String oldPassword;
+    @NotBlank
+    @Size(min = 8, message = "password cannot be shorter, than 8 characters")
+    String newPassword;
+}
