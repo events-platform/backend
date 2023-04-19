d854db395538cb1522a1868025c6ef7ca3bda1a5
nobodyknowsdotcom <nobodyknowsdotcom@gmail.com>
2023-04-19 14:39:25 +0500

    Setup mapstruct

--- src/main/java/com/example/eventsplatformbackend/mapper/UserMapper.java
+++ src/main/java/com/example/eventsplatformbackend/mapper/UserMapper.java
@@ -1,19 +1,21 @@
 package com.example.eventsplatformbackend.mapper;
 
+import com.example.eventsplatformbackend.domain.dto.request.RegistrationDto;
 import com.example.eventsplatformbackend.domain.dto.request.UserEditDto;
 import com.example.eventsplatformbackend.domain.entity.User;
 import org.mapstruct.*;
 import org.springframework.beans.factory.annotation.Value;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public abstract class UserMapper {
     @Value("${server.default-avatar-dir}")
-    private final String defaultAvatarsDirectory;
-
-    protected UserMapper(String defaultAvatarsDirectory) {
-        this.defaultAvatarsDirectory = defaultAvatarsDirectory;
-    }
-
+    private String defaultAvatarDir;
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    public abstract void updateUserFromDto(UserEditDto dto, @MappingTarget User entity);
+    public abstract void updateUserFromUserEditDto(UserEditDto dto, @MappingTarget User entity);
+    @Mapping(target = "avatar", expression = "java(this.getDefaultAvatarDir())")
+    public abstract User registrationDtoToUser(RegistrationDto registrationDto);
+
+    public String getDefaultAvatarDir() {
+        return defaultAvatarDir;
+    }
 }
