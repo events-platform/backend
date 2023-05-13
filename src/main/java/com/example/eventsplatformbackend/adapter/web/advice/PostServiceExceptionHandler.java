27409516b11ecfffc05a8aa66be097efa8ca8942
Dmitry Medvedev <75564672+nobodyknowsdotcom@users.noreply.github.com>
2023-05-14 01:19:51 +1000

    Add endpoint to fetch all posts, russificate post errors for client (#29)
    * Add all posts endpoint, add posts response russification
    
    * Set new version
    
    ---------
    
    Co-authored-by: nobodyknowsdotcom <nobodyknowsdotcom@gmail.com>

--- src/main/java/com/example/eventsplatformbackend/adapter/web/advice/PostServiceExceptionHandler.java
+++ src/main/java/com/example/eventsplatformbackend/adapter/web/advice/PostServiceExceptionHandler.java
@@ -2,31 +2,45 @@ package com.example.eventsplatformbackend.adapter.web.advice;
 
 import com.example.eventsplatformbackend.exception.InvalidDateException;
 import com.example.eventsplatformbackend.exception.PostAlreadyExistsException;
-import org.springframework.http.HttpStatus;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.stream.Collectors;
+import java.util.List;
 
 @ControllerAdvice
 public class PostServiceExceptionHandler {
+    private static final String CONTENT_TYPE = "Content-Type";
+    private static final String CONTENT_TYPE_VALUE =  "text/html; charset=utf-8";
     @ExceptionHandler(PostAlreadyExistsException.class)
     public ResponseEntity<String> handlePostAlreadyExistsException(PostAlreadyExistsException e){
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+        return ResponseEntity
+                .status(409)
+                .header(CONTENT_TYPE, CONTENT_TYPE_VALUE)
+                .body(e.getMessage());
     }
     @ExceptionHandler(org.springframework.validation.BindException.class)
     public ResponseEntity<String> handleBindException(BindException e){
-        String reportTemplate = "Problem with %s, error message: %s \n";
-        return ResponseEntity.unprocessableEntity()
-                .header("Content-Type", "text/html; charset=utf-8")
-                .body(e.getFieldErrors().stream()
-                        .map(err -> String.format(reportTemplate, err.getField(), err.getDefaultMessage()))
-                        .collect(Collectors.joining()));
+        BindingResult result = e.getBindingResult();
+        List<FieldError> fieldErrors = result.getFieldErrors();
+        List<String> messages = fieldErrors.stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+        String message = String.join("\n", messages);
+        return ResponseEntity
+                .status(422)
+                .header(CONTENT_TYPE, CONTENT_TYPE_VALUE)
+                .body(message);
     }
     @ExceptionHandler(InvalidDateException.class)
     public ResponseEntity<String> handleInvalidDateException (InvalidDateException e){
-        return ResponseEntity.badRequest().body(e.getMessage());
+        return ResponseEntity
+                .status(400)
+                .header(CONTENT_TYPE, CONTENT_TYPE_VALUE)
+                .body(e.getMessage());
     }
 }
