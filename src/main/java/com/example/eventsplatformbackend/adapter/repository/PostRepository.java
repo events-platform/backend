c2f3cf11fdc471b71e60d1250502fa51bd3c7735
Dmitry Medvedev <75564672+nobodyknowsdotcom@users.noreply.github.com>
2023-05-16 09:24:46 +1000

    Use LocalDateTime as post beginDate and endDate (#39)

--- src/main/java/com/example/eventsplatformbackend/adapter/repository/PostRepository.java
+++ src/main/java/com/example/eventsplatformbackend/adapter/repository/PostRepository.java
@@ -4,9 +4,9 @@ import com.example.eventsplatformbackend.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    boolean existsPostByBeginDateAndName(LocalDate beginDate, String name);
+    boolean existsPostByBeginDateAndName(LocalDateTime beginDate, String name);
 }
