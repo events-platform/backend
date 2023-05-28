05782dcef68d8c7f6657dd8a56f24d708924b3c1
Dmitry Medvedev <75564672+nobodyknowsdotcom@users.noreply.github.com>
2023-05-29 00:44:07 +0500

    Posts filtering (#52)
    * Add posts filtering by begin and end date
    
    * Update version
    
    ---------
    
    Co-authored-by: nobodyknowsdotcom <nobodyknowsdotcom@gmail.com>

--- src/main/java/com/example/eventsplatformbackend/adapter/web/advice/DatabaseExceptionHandler.java
+++ src/main/java/com/example/eventsplatformbackend/adapter/web/advice/DatabaseExceptionHandler.java
@@ -2,6 +2,8 @@ package com.example.eventsplatformbackend.adapter.web.advice;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
+import org.postgresql.util.PSQLException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,5 +21,17 @@ public class DatabaseExceptionHandler {
         return ResponseEntity
                 .status(422)
                 .body(String.join("\n", String.join("\n", errors)));
+    }
+    @ExceptionHandler(PSQLException.class)
+    public ResponseEntity<String> handlePSQLError(PSQLException e){
+        return ResponseEntity
+                .status(400)
+                .body(e.getServerErrorMessage().getMessage());
+    }
+    @ExceptionHandler(InvalidDataAccessApiUsageException.class)
+    public ResponseEntity<String> handleInvalidDataAccessApiUsageException(InvalidDataAccessApiUsageException e){
+        return ResponseEntity
+                .status(400)
+                .body(e.getMessage());
     }
 }
--- src/test/java/com/example/eventsplatformbackend/AuthTest.java
+++ src/test/java/com/example/eventsplatformbackend/AuthTest.java
@@ -4,7 +4,6 @@ import com.example.eventsplatformbackend.adapter.objectstorage.S3Adapter;
 import com.example.eventsplatformbackend.config.AwsConfig;
 import com.example.eventsplatformbackend.config.AwsCredentials;
 import com.example.eventsplatformbackend.domain.dto.response.JwtResponse;
-import com.example.eventsplatformbackend.domain.entity.User;
 import com.example.eventsplatformbackend.security.JwtUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Order;
@@ -19,9 +18,7 @@ import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.util.AssertionErrors.assertEquals;
 import static org.springframework.test.util.AssertionErrors.assertNotNull;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
@@ -42,14 +39,8 @@ class AuthTest extends TestParent{
     private ObjectMapper objectMapper;
 
     @Test
-    void shouldGetNotFound() throws Exception {
-        mockMvc.perform(get("/user/username123"))
-                .andDo(print())
-                .andExpect(status().isNotFound());
-    }
-    @Test
     @Order(1)
-    void registerUser_getJwtToken() throws Exception {
+    void registerUser() throws Exception {
         Map<String,Object> body = new HashMap<>();
         body.put("username",username);
         body.put("email",email);
@@ -63,9 +54,8 @@ class AuthTest extends TestParent{
 
         verify(s3Adapter).getLink(anyString());
     }
-
     @Test
-    void loginWithWrongEmail_get422() throws Exception {
+    void loginWithMalformedEmail_get422() throws Exception {
         Map<String,String> body = new HashMap<>();
         body.put("email","wrongEmail");
         body.put("password",password);
@@ -77,9 +67,8 @@ class AuthTest extends TestParent{
                 .andExpect(status().is(422))
                 .andReturn();
     }
-
     @Test
-    void login_getJwt() throws Exception {
+    void login_getJwt_extractUserDataFromJwt() throws Exception {
         Map<String,String> body = new HashMap<>();
         body.put("email",email);
         body.put("password",password);
@@ -94,33 +83,5 @@ class AuthTest extends TestParent{
                 .getContentAsString();
         JwtResponse jwtResponse = new ObjectMapper().readValue(response, JwtResponse.class);
         assertNotNull("token should return owner's id!", jwtUtil.extractUserId(jwtResponse.getAccessToken()));
-    }
-
-    @Test
-    void login_thenFetchUser() throws Exception {
-        Map<String,String> body = new HashMap<>();
-        body.put("email",email);
-        body.put("password",password);
-
-        String response = mockMvc.perform(post("/auth/login")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(body))
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andReturn()
-                .getResponse()
-                .getContentAsString();
-        JwtResponse jwtResponse = new ObjectMapper().readValue(response, JwtResponse.class);
-        assertNotNull("token should return owner's id", jwtUtil.extractUserId(jwtResponse.getAccessToken()));
-
-        String userJson = mockMvc.perform(get("/user/" + username)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andReturn()
-                .getResponse()
-                .getContentAsString();
-        User user = new ObjectMapper().readValue(userJson, User.class);
-
-        assertEquals("username should be equals", username, user.getUsername());
     }
 }
