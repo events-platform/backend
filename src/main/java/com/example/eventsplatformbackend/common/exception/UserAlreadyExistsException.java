a7320acb48dfdfffa13f0ce6fdb766babbf22ecd
Dmitry Medvedev <myclosestisa@gmail.com>
2023-05-31 10:50:47 +0500

    Refactor AuthController and AuthService

--- src/main/java/com/example/eventsplatformbackend/common/exception/UserAlreadyExistsException.java
+++ src/main/java/com/example/eventsplatformbackend/common/exception/UserAlreadyExistsException.java
@@ -1,6 +1,6 @@
 package com.example.eventsplatformbackend.common.exception;
 
-public class UserAlreadyExistsException extends Exception{
+public class UserAlreadyExistsException extends RuntimeException{
     public UserAlreadyExistsException(String message){
         super(message);
     }
