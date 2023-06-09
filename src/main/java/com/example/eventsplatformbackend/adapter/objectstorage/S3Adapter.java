ef1f36f3581cd21abe435491798317e75a4fb3f2
Dmitry Medvedev <myclosestisa@gmail.com>
2023-06-09 05:22:02 +0500

    Set random avatar to new users

--- src/main/java/com/example/eventsplatformbackend/adapter/objectstorage/S3Adapter.java
+++ src/main/java/com/example/eventsplatformbackend/adapter/objectstorage/S3Adapter.java
@@ -1,16 +1,19 @@
 package com.example.eventsplatformbackend.adapter.objectstorage;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.*;
 import com.example.eventsplatformbackend.config.AwsCredentials;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Выполняет операции над файлами в объектном хранилище.
  * Умеет сохранять, скачивать и удалять файлы из хранилища.
@@ -34,6 +37,26 @@ public class S3Adapter {
             InputStream inputStream) {
         log.info("Saving {} to S3", path);
         amazonS3.putObject(awsCredentials.getBucketName(), path, inputStream, objectMetadata);
+    }
+
+    /**
+     * Возвращает список всех объектов в директории
+     * @param prefix Префикс элементов, которые необходимо найти
+     * @return Список объектов по указанному префиксу
+     */
+    @Cacheable(value = "objectSummaries")
+    public List<String> getObjectSummaries(String prefix){
+        List<String> summaries = new ArrayList<>();
+
+        ListObjectsV2Request req = new ListObjectsV2Request()
+                .withBucketName(awsCredentials.getBucketName())
+                .withPrefix(prefix)
+                .withDelimiter("/");
+        ListObjectsV2Result listing = amazonS3.listObjectsV2(req);
+        for (S3ObjectSummary summary: listing.getObjectSummaries()) {
+            summaries.add(summary.getKey());
+        }
+        return summaries;
     }
 
     public S3Object getFile(String filename) {
